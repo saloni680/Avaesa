6c572613fba0477b2cb522c4d714801b560677ed
Saloni Chourasiya <chourasiyasaloni7901@gmail.com>
2024-03-18 12:46:32 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import Navbar from './Navbar'
+import Mainn from './Mainn'
+
+const App = () => {
+  
+
+  return (
+    <div>
+        <Navbar/>
+        <Mainn/>
+
+    </div>
+  )
+}
+
+export default App
